459868e11d27dd1b54b5b67e52d8bf86547e576c
Dicky <Dickyputra.tp21@mail.mmtc.ac.id>
2022-09-13 13:23:58 +0700

    Upload

---
+++ Assets/Scripts/BatasAkhirSampah.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BatasAkhirSampah : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+	void OntriggerEnter2D (Collider2D Collision)
+	{
+		Destroy (Collision.gameObject);
+		SceneManager.LoadScene ("GameOver"); // Baris ini akan digunakan pada submodul GameOver
+
+	}
+}
